b5e2add96fe8b4ad85538d998d5494627ff048a1
Josef Saltz <jpsaltz@gmail.com>
2018-01-09 21:04:34 -0800

    Adding and polishing of more styles

--- twitch-tracker/script.js
+++ twitch-tracker/script.js
@@ -1,7 +1,17 @@
-var add_streamer = document.querySelector(".add-streamer button");
-
+const add_streamer = document.querySelector(".add-streamer button");
+console.log(add_streamer);
 add_streamer.addEventListener("click", function() {
   console.log("Click!");
+});
+
+add_streamer.addEventListener("mouseover", function() {
+  add_streamer.style.transform = "scaleZ(20%)";
+  console.log(JSON.stringify(add_streamer.style.transform));
+});
+
+add_streamer.addEventListener("mouseleave", function() {
+  add_streamer.style.transform = "scaleZ(20%)";
+  console.log(JSON.stringify(add_streamer.style.transform));
 });
 
 function create_query_form() {
